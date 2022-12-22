0d28657f9f4dc694c5367886742a9ecb09835709
Anton Edholm <50101559+Draggel12345@users.noreply.github.com>
2022-12-22 15:13:02 +0100

    Splite interface and class

--- Data/Services/IPersonService.cs
+++ Data/Services/IPersonService.cs
@@ -1,5 +1,4 @@
 ﻿using RandomMockUpTest.Data.Models;
-using RandomPersonInfoGenerator.Data.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,80 +10,5 @@ namespace RandomMockUpTest.Data.Services
     public interface IPersonService
     {
         List<Person> CreateRandomPeopleByAmount(int amount);
-    }
-
-    public class PersonService : IPersonService
-    {
-        private readonly RandomInfoService service = new();
-
-        /// <summary>
-        /// <Author>Mike Polen</Author>
-        /// https://stackoverflow.com/questions/9/how-do-i-calculate-someones-age-based-on-a-datetime-type-birthday
-        /// </summary>
-        /// <param name="dateOfBirth"></param>
-        /// <DateTime name="today"></DateTime>
-        /// <returns name="age"></returns>
-        private static int DateOfBirthConvertedToAge(DateTime dateOfBirth)
-        {
-            DateTime today = DateTime.Today;
-            int age = today.Year - dateOfBirth.Year;
-
-            if (dateOfBirth.Date > today.AddYears(-age))
-            {
-                age--;
-            }
-            return age;
-        }
-
-        //Returns a new random Person(Male) or Person(Female).
-        public List<Person> CreateRandomPeopleByAmount(int amount)
-        {
-            int id = 0;
-            List<Person> result = new();
-            if (amount > 0)
-            {
-                for (int i = 0; i < amount; i++)
-                {
-                    DateTime dateOfBirth = service.GetRandomDateOfBirth();
-                    string gender = service.GetRandomGender();
-                    //The switch use the random gender-value to set the correct name and gender.ToLower() to the Person-object. 
-                    switch (gender)
-                    {
-                        case "Male":
-                            {
-                                result.Add(new Person
-                                {
-                                    Id = ++id,
-                                    FirstName = service.GetRandomMaleFirstName(),
-                                    LastName = service.GetRandomLastName(),
-                                    Age = DateOfBirthConvertedToAge(dateOfBirth),
-                                    DateOfBirth = dateOfBirth,
-                                    Gender = gender
-                                });
-                                break;
-                            }
-                        case "Female":
-                            {
-                                result.Add(new Person
-                                {
-                                    Id = ++id,
-                                    FirstName = service.GetRandomFemaleFirstName(),
-                                    LastName = service.GetRandomLastName(),
-                                    Age = DateOfBirthConvertedToAge(dateOfBirth),
-                                    DateOfBirth = dateOfBirth,
-                                    Gender = gender
-                                });
-                                break;
-                            }
-                    }
-                }
-            }
-            else
-            {
-                throw new ArgumentException("Value can't be lower than 1.", nameof(amount));
-            }
-
-            return result;
-        }
     }
 }
---
+++ Data/Services/PersonService.cs
@@ -0,0 +1,80 @@
+﻿using RandomMockUpTest.Data.Models;
+using RandomPersonInfoGenerator.Data.Services;
+
+namespace RandomMockUpTest.Data.Services
+{
+    public class PersonService : IPersonService
+    {
+        private readonly RandomInfoService service = new();
+
+        /// <summary>
+        /// <Author>Mike Polen</Author>
+        /// https://stackoverflow.com/questions/9/how-do-i-calculate-someones-age-based-on-a-datetime-type-birthday
+        /// </summary>
+        /// <param name="dateOfBirth"></param>
+        /// <DateTime name="today"></DateTime>
+        /// <returns name="age"></returns>
+        private static int DateOfBirthConvertedToAge(DateTime dateOfBirth)
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - dateOfBirth.Year;
+
+            if (dateOfBirth.Date > today.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
+
+        //Returns a new random Person(Male) or Person(Female).
+        public List<Person> CreateRandomPeopleByAmount(int amount)
+        {
+            int id = 0;
+            List<Person> result = new();
+            if (amount > 0)
+            {
+                for (int i = 0; i < amount; i++)
+                {
+                    DateTime dateOfBirth = service.GetRandomDateOfBirth();
+                    string gender = service.GetRandomGender();
+                    //The switch use the random gender-value to set the correct name and gender.ToLower() to the Person-object. 
+                    switch (gender)
+                    {
+                        case "Male":
+                            {
+                                result.Add(new Person
+                                {
+                                    Id = ++id,
+                                    FirstName = service.GetRandomMaleFirstName(),
+                                    LastName = service.GetRandomLastName(),
+                                    Age = DateOfBirthConvertedToAge(dateOfBirth),
+                                    DateOfBirth = dateOfBirth,
+                                    Gender = gender
+                                });
+                                break;
+                            }
+                        case "Female":
+                            {
+                                result.Add(new Person
+                                {
+                                    Id = ++id,
+                                    FirstName = service.GetRandomFemaleFirstName(),
+                                    LastName = service.GetRandomLastName(),
+                                    Age = DateOfBirthConvertedToAge(dateOfBirth),
+                                    DateOfBirth = dateOfBirth,
+                                    Gender = gender
+                                });
+                                break;
+                            }
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Value can't be lower than 1.", nameof(amount));
+            }
+
+            return result;
+        }
+    }
+}
